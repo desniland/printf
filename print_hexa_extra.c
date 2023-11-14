abf6a9295eb41f38702bbd9085595d6ebc634c9a
desniland <albertdesmond26@gmail.com>
2023-11-14 11:28:25 +0300

    add hexa_extra and HEXA_EXTRA

--- print_hexa_extra.c
+++ print_hexa_extra.c
@@ -28,7 +28,7 @@ int print_hexa_extra(unsigned long int num)
 	for (x = track - 1; x >= 0; x--)
 	{
 		if (array[x] > 9)
-			array[x] = array[x] + 7;
+			array[x] = array[x] + 39;
 		_putchar(array[x] + '0');
 	}
 	free(array);
