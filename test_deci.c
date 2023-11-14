c49f5f2f81955b4117254fb2d8cae65b14df4630
desniland <albertdesmond26@gmail.com>
2023-11-14 18:28:37 +0300

    commit changes

---
+++ test_deci.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+
+#include "main.h"
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *    */
+int main(void)
+{
+int n = printf("%f", 2.35);
+printf("\nn = %d \n", n);
+
+return (0);
+}
