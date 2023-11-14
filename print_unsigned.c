a7e2b750d71f10f9afd355ad0521ed6a6bbb75e2
desniland <albertdesmond26@gmail.com>
2023-11-14 11:08:38 +0300

    add print_unsigned.c

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * printf_unsigned - prints unsigned integer
  * @args: argument to print
@@ -9,34 +8,37 @@ int printf_unsigned(va_list args)
 {
 	unsigned int x = va_arg(args, unsigned int);
 	int num, last = x % 10, digit, exp = 1;
-	int y = 1;
+	int  y = 1;
 
 	x = x / 10;
 	num = x;
 
-	if (x == 0)
+	if (last < 0)
 	{
-		_putchar('0');
-		return 1;
-	}
-
-	while (num > 0)
-	{
-		exp = exp * 10;
-		num = num / 10;
-	}
-
-	num = x;
-	while (exp > 0)
-	{
-		digit = num / exp;
-		_putchar(digit + '0');
-		num = num - (digit * exp);
-		exp = exp / 10;
+		_putchar('-');
+		num = -num;
+		x = -x;
+		last = -last;
 		y++;
 	}
-
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = x;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			y++;
+		}
+	}
 	_putchar(last + '0');
 
-	return y;
+	return (y);
 }
